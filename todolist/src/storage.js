594af5558d02479121bd55119c0351a85d02d338
zhaochenmin <zhaochenmin@dpandora.cn>
2017-09-19 12:13:45 +0800

    commit

--- todolist/src/storage.js
+++
@@ -1,9 +0,0 @@
-const storageKey='vue_todo';
-export default {
-  get:function () {
-    return JSON.parse(window.localStorage.getItem(storageKey)||'[]')
-  },
-  set:function (items) {
-    window.localStorage.setItem(storageKey,JSON.stringify(items))
-  }
-}
